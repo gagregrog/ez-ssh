b8703c337854d98c7b7bfe49cde443981d640435
Robert Reed <robert.mc.reed@gmail.com>
2018-10-06 18:11:55 -0700

    linebreaks in loggers, handleList helper

--- src/lib/util.js
+++ src/lib/util.js
@@ -2,9 +2,9 @@
 
 const util = module.exports = {};
 
-util.log = (...args) => console.log('[INFO]', ...args);
+util.log = (...args) => console.log('\n[INFO]', ...args);
 
-util.err = (...args) => console.error('[ERROR]', ...args);
+util.err = (...args) => console.error('\n[ERROR]', ...args);
 
 util.handleError = (...messages) => {
   messages.forEach((message) => {
@@ -27,3 +27,17 @@ util.hasArgs = (options) => {
 };
 
 util.isCalling = (options) => (Object.keys(options).length === 1 && !!options.key);
+
+util.handleList = (options, keys, keyNames) => {
+  if (!keyNames.length) util.log('You have no saved keys.', '\n\tType "ez-ssh -h" for help getting started.');
+  else {
+    util.log('Your saved keys are as follows:\n');
+
+    if (options.verbose) {
+      const max = keyNames.reduce((a, c) => (a > c.length ? a : c.length));
+      keyNames.forEach((key) => {
+        console.log(`\t${key.padEnd(max)}: ${keys[key]}`);
+      });
+    } else console.log(`\t${keyNames.join(', ')}`);
+  }
+};
