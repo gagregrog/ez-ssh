049ced44736ba363c35e87f7d1d76c5cdcd3ac64
Robert Reed <robert.mc.reed@gmail.com>
2018-10-06 21:42:53 -0700

    index finished

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 
 const main = require('./src/main');
 const { options, usage, error } = require('./src/lib/cli');
-const { err, hasArgs, handleError } = require('./src/lib/util');
+const { hasArgs, handleError } = require('./src/lib/util');
 
-if (error) err(error);
-else if (options.help || !hasArgs(options)) console.log(usage);
+if (error) handleError(error.message);
+else if (options.help || !hasArgs(options) || options.key === 'help') console.log(usage);
 else if (!(options.key || options.list)) handleError('You must provide a keyword when adding or running a command.');
 else main(options);
