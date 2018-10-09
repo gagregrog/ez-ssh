bbbbf382c11b6f88aec2ba95d869a6a4b694eca6
Robert Reed <robert.mc.reed@gmail.com>
2018-10-09 15:25:44 -0700

    on no name pass through to main

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const main = require('./src/main');
 const { options, usage, error } = require('./src/lib/cli');
 const { hasArgs, handleError } = require('./src/lib/util');
 
+const badUsage = hasArgs(options) && !options.name && !(options.list || options.wizard);
+const showUsage = (options.help || ['help', 'h'].includes((options.name || '').toLowerCase()));
+
 if (error) handleError(error.message);
-else if (options.help || !hasArgs(options) || options.name === 'help') console.log(usage);
-else if (!(options.name || options.list)) handleError('You must provide a name when adding or running a command.');
+else if (showUsage) console.log(usage);
+else if (badUsage) handleError('You must provide a name when adding or executing an alias.');
 else main(options);
